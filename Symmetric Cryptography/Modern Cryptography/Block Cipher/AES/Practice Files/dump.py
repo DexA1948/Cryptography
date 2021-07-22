e05c59fef5aad153952b6944e82ed1d8e9c28187
DexA1948 <VCIT06@vedascollege.edu.np>
2021-07-23 00:45:57 +0545

    Added AES, rough files and Lab Work

---
+++ Symmetric Cryptography/Modern Cryptography/Block Cipher/AES/Practice Files/dump.py
@@ -0,0 +1,190 @@
+"""
+
+round_key = []
+
+
+def formroundkeys(key):
+    print("key", key)
+    round_key.append(key)
+
+    wordlist = [[]]*44
+
+    for row_ in range(0, 4):
+        for col_ in range(0, 4):
+            print("value: ", key[col_][row_])
+            wordlist[0].append(key[col_][row_])
+        print("wordlist: ", wordlist)
+    print(wordlist)
+
+
+def formmatrix(given):
+    matrix_ = [[], [], [], []]
+
+    for row in range(4):
+        for col in range(4):
+            matrix_[col].append(ord(given[row * 4 + col]))
+
+    return matrix_
+
+
+key_ = formmatrix("Thats my Kung Fu")
+print(key_)
+
+for i in range(0, 4):
+    for j in range(0, 4):
+        print(key_[i][j], end=" ")
+    print("\n")
+
+for i in range(0, 4):
+    for j in range(0, 4):
+        print(round_key[0][i][j], end=" ")
+    print("\n")
+
+print("roundkey", round_key)
+
+formroundkeys(key_)
+
+"""
+
+
+def formmatrix(given):
+    matrix_ = [[], [], [], []]
+
+    for row in range(4):
+        for col in range(4):
+            matrix_[col].append(ord(given[row * 4 + col]))
+
+    return matrix_
+
+
+round_key = [
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+]
+
+word = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+print(round_key[0][0][0], round_key[0][0][1])
+round_key[0][0][1] = 90
+print(round_key)
+
+text = "Thats my Kung Fu"
+key_ = formmatrix(text)
+print("key0: ", key_)
+
+for i in range(0, 4):
+    for j in range(0, 4):
+        round_key[0][i][j] = key_[i][j]
+
+key_[0][0] = 90
+
+print(round_key)
